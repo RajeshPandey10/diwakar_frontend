febeff9d157795f7b92d7dcff96a655fc48cfc22
RajeshPandey10 <imrajesh2005@gmail.com>
2025-04-19 18:41:47 +0545

    commit-comit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       external: ['aos']
@@ -11,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      axios: require.resolve('axios'), 
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
